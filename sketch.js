43b56cb71601f6e33cd97b9562d75ccc4abf0d04
kushtiwari2jan <75683653+kushtiwari2jan@users.noreply.github.com>
2021-01-18 19:58:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var canvas,sarface1,sarface2,sarface3,sarface4;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    //create 4 different surfaces
+sarface1=createsprite(750,599,25,80)
+
+
+    //create box sprite and give velocity
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    //create edgeSprite
+
+
+
+    //add condition to check if box touching surface and make it box
+    drawSprites();
+}
